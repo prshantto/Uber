c74ba2bdab2ba87849c042f69544dedf2bface72
Prashant Singh <prashanttt001@gmail.com>
2024-12-17 10:34:01 +0530

    minor changes

--- frontend/src/pages/CaptainLogout.jsx
+++ frontend/src/pages/CaptainLogout.jsx
@@ -1,29 +1,29 @@
-import React, { useEffect } from 'react'
-import { useNavigate } from 'react-router-dom';
-import axios from 'axios';
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 const CaptainLogout = () => {
-    const navigate = useNavigate();
-    const token = localStorage.getItem("token");
-    
+  const navigate = useNavigate();
+  const token = localStorage.getItem("token");
+
+  axios
+    .get(`${import.meta.env.VITE_BASE_URL}/user/logout`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+    .then((response) => {
+      if (response.status === 200) {
+        localStorage.removeItem("token");
+        localStorage.removeItem("user");
+      }
+    });
 
   useEffect(() => {
-    axios
-      .get(`${import.meta.env.VITE_BASE_URL}/user/logout`, {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      })
-      .then((response) => {
-        if (response.status === 200) {
-          localStorage.removeItem("token");
-          localStorage.removeItem("user");
-        }
-      });
     navigate("/login");
   }, [token, navigate]);
 
   return <div>User Logedout Sucessfully</div>;
-}
+};
 
-export default CaptainLogout
+export default CaptainLogout;
